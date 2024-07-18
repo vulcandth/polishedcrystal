7144df29145617c3f7ba35cc5d6dfc0a32c4513a
vulcandth <vulcandth@gmail.com>
2024-07-17 21:19:50 -0500

    Add patching indicator.

--- tools/polished_save_patcher/src/main.cpp
+++ tools/polished_save_patcher/src/main.cpp
@@ -11,33 +11,37 @@
 #include <vector>
 
 extern "C" {
-    void patch_save(const std::string &old_save_path, const std::string &new_save_path) {
+    emscripten::val patch_save(const std::string &old_save_path, const std::string &new_save_path) {
+        // Result object to return to JavaScript
+        emscripten::val result = emscripten::val::object();
+        bool success = true;
+
         // Load the old save file
         SaveBinary oldSave(old_save_path);
         // copy the old save file to the new save file
         SaveBinary newSave = oldSave;
         // load the save version big endian word
+
         uint16_t saveVersion = oldSave.getWordBE(SAVE_VERSION_ABS_ADDRESS);
-        // if save versions < 7 is unsupported. if save version is > 8, it is unsupported. if save version is 8, it is already patched. 
         if (saveVersion != 0x07) {
             js_error << "Unsupported save version: " << std::hex << saveVersion << std::endl;
-            return;
+            success = false;
+        } else {
+            if (!patchVersion7to8(oldSave, newSave)) {
+                js_error << "Failed to patch save file." << std::endl;
+                success = false;
+            } else {
+                js_info << "Saving file..." << std::endl;
+                newSave.save(new_save_path);
+                js_info << "File saved successfully!" << std::endl;
+            }
         }
 
-        if(!patchVersion7to8(oldSave, newSave)) {
-            js_error << "Failed to patch save file." << std::endl;
-            return;
-        }
-
-        // write that we are saving the file
-        js_info << "Saving file..." << std::endl;
-        newSave.save(new_save_path);
-        // write that the file was saved
-        js_info << "File saved successfully!" << std::endl;
+        result.set("success", success);
+        return result;
     }
 }
 
-// Binding the patch_save function to JavaScript
 EMSCRIPTEN_BINDINGS(patch_save_module) {
     emscripten::function("patch_save", &patch_save);
 }
